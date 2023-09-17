aa8c8708f52aeb7c5d61255521e5664956b81193
LDM <logandhi64@gmail.com>
2023-09-16 22:30:14 -0400

    Finished file chunking (1 MB)

--- src/main/java/net/ldm/mod_link/ModLinkServer.java
+++ src/main/java/net/ldm/mod_link/ModLinkServer.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.ldm.mod_link.networking.packet.PacketChannels;
 import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,50 +32,40 @@ public class ModLinkServer implements DedicatedServerModInitializer {
 		});
 	}
 
-	private Set<byte[]> readMods() throws IOException {
+	private @NotNull Set<byte[]> readMods() throws IOException {
 		Set<byte[]> out = new HashSet<>();
 		for (File modFile: Objects.requireNonNull(MODS_DIR.listFiles())) {
 			byte[] fileData = FileUtils.readFileToByteArray(modFile);
 			byte[] fileNameBytes = modFile.getName().getBytes();
 
-			int bufferSize = START_OF_HEADER.length + fileNameBytes.length + START_OF_FILE.length + fileData.length + END_OF_FILE.length;
+			byte[] result = new byte[START_OF_HEADER.length + fileNameBytes.length + START_OF_FILE.length + fileData.length + END_OF_FILE.length];
 
-			int maxChunks = (bufferSize + 999999) / 1000000;
+			int index = 0;
 
+			System.arraycopy(START_OF_HEADER, 0, result, index, START_OF_HEADER.length);
+			index += START_OF_HEADER.length;
 
-			for (int chunk = 0; chunk < maxChunks; chunk++) {
-				int chunkStart = chunk * 1000000;
-				int chunkEnd = Math.min((chunk + 1) * 1000000, bufferSize);
+			System.arraycopy(fileNameBytes, 0, result, index, fileNameBytes.length);
+			index += fileNameBytes.length;
 
-				byte[] result = new byte[bufferSize];
-				int index = 0;
+			System.arraycopy(START_OF_FILE, 0, result, index, START_OF_FILE.length);
+			index += START_OF_FILE.length;
 
-				int min = Math.min(START_OF_HEADER.length - chunkStart, result.length);
-				if (chunkStart < min) {
-					System.arraycopy(START_OF_HEADER, chunkStart, result, index, min);
-					index += min;
-				} else if ((chunkStart - START_OF_HEADER.length) < fileNameBytes.length) {
-					int copyLength = Math.min(fileNameBytes.length - (chunkStart - START_OF_HEADER.length), result.length);
-					System.arraycopy(fileNameBytes, (chunkStart - START_OF_HEADER.length), result, index, copyLength);
-					index += copyLength;
-				} else if ((chunkStart - START_OF_HEADER.length - fileNameBytes.length) < START_OF_FILE.length) {
-					int copyLength = Math.min(START_OF_FILE.length - (chunkStart - START_OF_HEADER.length - fileNameBytes.length), result.length);
-					System.arraycopy(START_OF_FILE, (chunkStart - START_OF_HEADER.length - fileNameBytes.length), result, index, copyLength);
-					index += copyLength;
-				} else if ((chunkStart - START_OF_HEADER.length - fileNameBytes.length - START_OF_FILE.length) < fileData.length) {
-					int copyLength = Math.min(fileData.length - (chunkStart - START_OF_HEADER.length - fileNameBytes.length - START_OF_FILE.length), result.length);
-					System.arraycopy(fileData, (chunkStart - START_OF_HEADER.length - fileNameBytes.length - START_OF_FILE.length), result, index, copyLength);
-					index += copyLength;
-				} else {
-					int i = chunkStart - START_OF_HEADER.length - fileNameBytes.length - START_OF_FILE.length - fileData.length;
-					if (i < END_OF_FILE.length) {
-						int copyLength = Math.min(END_OF_FILE.length - i, result.length);
-						System.arraycopy(END_OF_FILE, i, result, index, copyLength);
-						index += copyLength;
-					}
-				}
+			System.arraycopy(fileData, 0, result, index, fileData.length);
+			index += fileData.length;
 
-				out.add(result);
+			System.arraycopy(END_OF_FILE, 0, result, index, END_OF_FILE.length);
+
+			int splitLength = 1000000;
+			int numOfChunks = (int)Math.ceil((double)result.length / splitLength);
+			for (int i = 0; i < numOfChunks; i++) {
+				int start = i * splitLength;
+				int length = Math.min(result.length - start, splitLength);
+
+				byte[] part = new byte[length];
+				System.arraycopy(result, start, part, 0, length);
+
+				out.add(part);
 			}
 		}
 		return out;
